726caec27fe230560709f13b2b37f99ee3cdd365
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-28 12:53:48 -0800

    maybe heroku deployed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/pure-dawn', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false } )
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
